e17336d16aff54f1ff5d42291e7db8a458ff3137
guille garcia <ggarciacardiel@gmail.com>
2022-11-28 15:33:22 +0100

    created obstacle class and added img in assets

--- js/assets.js
+++ js/assets.js
@@ -1,2 +1,7 @@
+//Player
 const surfer = new Image();
 surfer.src = '../img/player.png';
+
+//Surfboard Obstacles
+const surfBoard = new Image();
+surfBoard.src = '../img/surfboard.png';
---
+++ js/obstacles.js
@@ -0,0 +1,8 @@
+class Obstacle {
+  constructor () {
+    this.image = surfBoard;
+    this.width = 400;
+    this.height = 50;
+  }
+
+}
