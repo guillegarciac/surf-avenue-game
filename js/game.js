f49db5bc243223b743d6bf3afa6b821d66962a03
guille garcia <ggarciacardiel@gmail.com>
2022-12-03 21:40:09 +0100

    added levels logic to increase fallDown difficulty

--- js/game.js
+++ js/game.js
@@ -6,6 +6,7 @@ class Game {
     this.points = 0;
     this.generateInterval = null;
     this.currentTime = 0;
+    this.level = 1;
   }
 
   _timer() {
@@ -16,7 +17,7 @@ class Game {
 
   _generateObstacles() {
     this.generateInterval = setInterval (() => {
-      const newObstacle = new Obstacle();
+      const newObstacle = new Obstacle(this.level);
       newObstacle._assignImage();
       newObstacle._fallDown();
       this.obstacles.push(newObstacle);
@@ -64,7 +65,8 @@ class Game {
   _writeScore() {
     this.ctx.fillStyle = "white";
     this.ctx.font = "30px Poppins";
-    this.ctx.fillText(`Score: ${this.currentTime}`, 50, 40);
+    this.ctx.fillText(`Score: ${this.currentTime}`, 70, 40);
+    this.ctx.fillText(`Level: ${this.level}`, 800, 40)
   }
 
   _checkScore() {
@@ -84,10 +86,12 @@ class Game {
   _nextLevel() {
     clearInterval(this.generateInterval);
     clearInterval(this.intervalId);
+    this.level = this.level + 1;
     this.obstacles = [];
-    this.currentTime = 0;
     const levelPage = document.getElementById('level-page');
     levelPage.style = "display: flex";
+    document.getElementById('levelTag').innerHTML = `Let's try with Level ${this.level}`;
+    this.currentTime = 0;
     const canvas = document.getElementById('canvas');
     canvas.style = "display: none";
     const nextButton = document.getElementById('next');
@@ -118,9 +122,10 @@ class Game {
     clearInterval(this.generateInterval);
     clearInterval(this.intervalId);
     this.obstacles = [];
-    this.currentTime = 0;
     const losePage = document.getElementById('lose-page');
     losePage.style = "display: flex";
+    document.getElementById('pointsTag').innerHTML = `But congrats anyway you have reached Level ${this.level} with ${this.currentTime} points`;
+    this.currentTime = 0;
     const canvas = document.getElementById('canvas');
     canvas.style = "display: none";
     const restartButton = document.getElementById('restart');
--- js/obstacles.js
+++ js/obstacles.js
@@ -1,5 +1,5 @@
 class Obstacle {
-  constructor () {
+  constructor (level) {
     this.possiblePositions = [150, 450, 750];
     this.x = this.possiblePositions[Math.floor(Math.random() * 3)];
     this.y = -100;
@@ -7,6 +7,8 @@ class Obstacle {
     this.width = 180;
     this.height = 150;
     this.fallInterval = undefined;
+    this.speed = 5 + level
+    console.log(this.speed)
   }
 
   _fallDown() {
@@ -14,8 +16,8 @@ class Obstacle {
       if (this.y > 600) {
         clearInterval(this.fallInterval);
       }
-      this.y = this.y + 1;
-    }, 5)
+      this.y = this.y + this.speed;
+    }, 10)
   }
 
   _assignImage() {
